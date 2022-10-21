e39b7a39f3a92ff8150f325d42e9df1796b07a9d
Jefferson Boothe <46006304+shamuraii@users.noreply.github.com>
2022-10-20 20:59:04 -0400

    Basic file reading

--- tomasulo.py
+++ tomasulo.py
@@ -1,6 +1,13 @@
 def main():
-    print("ECE 2162 - Project 1","Jefferson Boothe","James Bickerstaff",sep="\n")
+    print("ECE 2162 - Project 1","Jefferson Boothe","James Bickerstaff","--------------------",sep="\n")
 
+    print("Loading instructions...")
+    f = open('instructions.txt', 'r')
+    inst_lines = f.readlines()
+    inst_lines = [s.lower().strip() for s in inst_lines]
+    for line in inst_lines:
+        print(line)
+    print("--------------------")
 
 if __name__ == '__main__':
     main()
