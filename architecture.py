7b892a7f5ab011d1d984dad31d1c2914fb256e44
James Bickerstaff <jjb169@pitt.edu>
2022-10-28 18:43:59 -0400

    Added ARFs and RAT, improved parsing of instrs

--- architecture.py
+++ architecture.py
@@ -1,3 +1,72 @@
+class IntegerARF:
+    registers = {}
+    def __init__(self, length) -> None:
+        #effectively add "length" number of registers
+        for i in range(length):
+            self.registers['R'+str(i)] = 0
+            
+    def update(self, register, value):
+        #first check for valid register
+        if register in self.registers:
+            #update the register value
+            self.registers[register] = value
+        else:
+            print("REGISTER " + register + " INVALID - CANNOT UPDATE ARF")
+            
+    def print(self):
+        #print all registers and their values
+        for key, value in self.registers.items():
+            print(key, ' : ', value)
+            
+class FloatingPointARF:
+    registers = {}
+    def __init__(self, length) -> None:
+        #effectively add "length" number of registers
+        for i in range(length):
+            self.registers['F'+str(i)] = 0.0
+            
+    def update(self, register, value):
+        #first check for valid register
+        if register in self.registers:
+            #update the register value
+            self.registers[register] = value
+        else:
+            print("REGISTER " + register + " INVALID - CANNOT UPDATE ARF")
+            
+    def print(self):
+        #print all registers and their values
+        for key, value in self.registers.items():
+            print(key, ' : ', value)
+            
+class RegisterAliasTable:
+    #using a dict here for unique-ness, can't have 2 R10s for instance
+    entries = {}
+    numIntegers = 0
+    numFloatingPoints = 0
+    def __init__(self, intNum, floatNum) -> None:
+        self.numIntegers = intNum
+        self.numFloatingPoints = floatNum
+        #loop through the number of int register entries & initialize RAT for that amount
+        for i in range(intNum):
+            self.entries['R'+str(i)] = 'R'+str(i)
+        #do the same for float regs
+        for i in range(floatNum):
+            self.entries['F'+str(i)] = 'F'+str(i)
+            
+    def update(self, register, newAlias):
+        #first check for valid register
+        if register in self.entries:
+            #update the register value
+            self.entries[register] = newAlias
+        else:
+            print("REGISTER " + register + " INVALID - CANNOT UPDATE RAT")
+            
+    def print(self):
+        #print all int reg aliases
+        for key, value in self.entries.items():
+            print(key, ' : ', value)
+
+
 class ReservationStation:
     def __init__(self) -> None:
         pass
--- tomasulo.py
+++ tomasulo.py
@@ -1,36 +1,98 @@
 import units
+import architecture
+
+class Instruction:
+    def __init__(self, type, field1, field2, field3):
+        self.type = type
+        self.field1 = field1
+        self.field2 = field2
+        self.field3 = field3
+        
+    def print(self):
+        if self.type != "NOP" and self.type != "SD" and self.type != "LD": 
+            print(self.type +' '+ self.field1 +','+ self.field2 +','+ self.field3)
+        elif self.type == "SD" or self.type == "LD":
+            print(self.type +' '+ self.field1 +','+ self.field2 +'('+ self.field3 + ')')
+        else:
+            print(self.type)
+
+def loadInstructions():
+    instructions = []
+    #open instruction file and read in all lines
+    print("Loading Instruction file...")
+    inst_file = open('instructions.txt', 'r')
+    inst_lines = inst_file.readlines()
+
+    #parse instructions
+    for line in inst_lines:    
+        #split each word by commas
+        parts = line.split(',')
+        #have to split the first part again into instr type and field 1
+        instrStart = parts[0].split(' ')
+        #grab the type of instruction first
+        type = instrStart[0].strip()
+        #create vars for the 3 fields Rdestination, Rsrc1, Rsrc2
+        field1 = None
+        field2 = None
+        field3 = None
+        if type == "NOP": #if NOP, just make all fields NOP
+            field1 = "NOP"
+            field2 = "NOP"
+            field3 = "NOP"
+        elif type == "LD" or type == "SD": #if load or store, need to manipulate some more
+            field1 = instrStart[1].strip() #grab the first register as normal
+            f2and3 = parts[1].split('(') #split the offset(R) term into [offset, R)]
+            field2 = f2and3[0].strip() #grab the 'offset' part and load it into field2
+            field3 = f2and3[1].replace(')', '').strip() #strip ')' from the R) factor and load into field3
+        else:
+            field1 = instrStart[1].strip() #grab destination register
+            field2 = parts[1].strip() #grab source 1 reg
+            field3 = parts[2].strip() #grab source 2 reg
+            
+        #add instruction once all parts are parsed
+        instructions.append(Instruction(type, field1, field2, field3))
+        
+    return instructions
+    
+def printInstructions(instructions):
+    print("Printing instructions...\n")
+    for curr in instructions:
+        curr.print()
+
 
 def main():
-    print("ECE 2162 - Project 1","Jefferson Boothe","James Bickerstaff","--------------------",sep="\n")
+    print("ECE 2162 - Project 1","Jefferson Boothe","James Bickerstaff","--------------------",sep="\n")    
 
-    print("Loading input file...")
-    f = open('instructions.txt', 'r')
-    inst_lines = f.readlines()
-    inst_lines = [s.lower().strip() for s in inst_lines]
+    print("Loading configuration file...")
+    f = open('config.txt', 'r')
+    config_lines = f.readlines()
+    config_lines = [s.lower().strip() for s in config_lines]
 
     #int adder, #rs, ex, mem, #fu
-    line = inst_lines[0].split(',')
+    line = config_lines[0].split(',')
     args = [eval(i) for i in line]
     intA = units.IntAdder(args[1],args[2],args[4])
     #fp adder, #rs, ex, mem, #fu
-    line = inst_lines[1].split(',')
+    line = config_lines[1].split(',')
     #fp multiplier, #rs, ex, mem, #fu
-    line = inst_lines[2].split(',')
+    line = config_lines[2].split(',')
     #load/store unit, #rs, ex, mem, #fu
-    line = inst_lines[3].split(',')
-
-    line = inst_lines[5].split(',')
+    line = config_lines[3].split(',')
     #rob,#entries
-    line = inst_lines[6].split(',')
+    line = config_lines[5].split(',')
     #cdb,#entries
-    line = inst_lines[7].split(',')
-    #HARD TO PARSE, REGISTER=VALUE
+    line = config_lines[6].split(',')
+    #HARD TO PARSE, REGISTER=VALUE -- still need to assign reg values to start
+    line = config_lines[7].split(',')
+    f.close()
 
-    #lines 9+ are instructions
-
-    for line in inst_lines:
-        print(line)
+    #call instruction method to read txt file
+    instructions = loadInstructions()
+    printInstructions(instructions)
+    
+    
     print("--------------------")
+    
 
 if __name__ == '__main__':
     main()
