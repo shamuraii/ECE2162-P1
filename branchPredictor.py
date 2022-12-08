5fa48f2303bf03025d74ac2f3fd707ba3f96a614
James Bickerstaff <jjb169@pitt.edu>
2022-12-08 18:06:03 -0500

    Fixed P1 BP returning PC vals

--- branchPredictor.py
+++ branchPredictor.py
@@ -1,74 +1,76 @@
 
 class BTBEntry:
-    def __init__(self):
-        self.PC = -1 #address/PC of the branch instruction
-        self.predictedPC = -1 #predicted PC to jump to if branch taken
-        self.branchPredict = -1 #simple one-bit predictor
-        
-    def __str__(self) -> str:
-        return "\t".join([str(self.PC), str(self.predictedPC), str(self.branchPredict)])
-        
-    #method to fill in fields of the BTBEntry 
-    def fillEntry(self, PC):
-        self.PC = PC
-        #not worrying about assigning predictedPC here as that will be done when updatePrediction is called right after this
-        #self.predictedPC = 0
-        self.branchPredict = 0 #doesn't matter what we set this to, it will be updated directly after populating entry
-    
-    #method to update the predicted branch result
-    def updatePrediction(self, branchResult, offset, mode):
-        #check the current 1-bit predictor state
-        if self.branchPredict == 1 and branchResult == 0: #if branch was predicted taken, but actually wasn't, update
-            self.branchPredict = 0
-            #also update the predicted PC to reflect this change
-            self.calculatePredictedPC(offset, mode)
-        elif self.branchPredict == 0 and branchResult == 1: #if branch was predicted not taken, but actually wasn't, update
-            self.branchPredict = 1
-            #also update the predicted PC to reflect this change
-            self.calculatePredictedPC(offset, mode)
-        #don't need to worry about the other cases since it's only a 1-bit predictor 
-    
-    #calculate the predicted PC - called upon filling BTBEntry, as well as when changing the prediction bit
-    def calculatePredictedPC(self, offset, mode):
-        #mode 1 for calculating: byte address calculation - only including this for if the professor uses byte addressed PC
-        if mode == 1:
-            #check if branch is predicted to be taken or not, and update predicted PC accordingly
-            if self.branchPredict == 1: #branch is predicted to be taken
-                #new PC = PC+4+offset<<2
-                bytePC = ((self.PC*4) + 4 + int(offset)) << 2 #byte addressed PC
-                self.predictedPC = bytePC / 4 #convert back to "regular" notation by dividing by 4, the number of bytes per PC entry
-            else: #else branch is predicted to be NOT taken
-                #new PC = PC+4
-                self.predictedPC = self.PC + 1 #not gonna bother with adding 4 then div by 4
-        else: #else, can just use normal "relative" addressing, ie PC goes 0,1,2,3,4,5,etc
-            #check if branch is predicted to be taken or not, and update predicted PC accordingly
-            if self.branchPredict == 1: #branch is predicted to be taken
-                #new PC = PC+1+offset
-                self.predictedPC = self.PC + 1 + int(offset) 
-                #print("self.predictedPC: ", self.predictedPC)
-            else: #else branch is predicted to be NOT taken
-                #new PC = PC + 1
-                self.predictedPC = self.PC + 1 
-            
-                
-        #not going to do the byte addressing [PC+4+offset<<2] method, just relative
-        #e.g. going from instr 7 to 0 -> 7+1-8=0, can use offset directly
-    
-    #set the predicted PC for this entry
-    def setPredictedPC(self, newPC):
-        self.predictedPC = newPC
-    
-    #grab PC for this entry
-    def getPC(self):
-        return self.PC
-        
-    #grab predicted PC for this entry
-    def getPredictedPC(self):
-        return self.predictedPC
-        
-    #grab the prediction for this branch - taken or not taken
-    def getPrediction(self):
-        return self.branchPredict   
+	def __init__(self):
+		self.PC = -1 #address/PC of the branch instruction
+		self.predictedPC = -1 #predicted PC to jump to if branch taken
+		self.branchPredict = -1 #simple one-bit predictor
+		
+	def __str__(self) -> str:
+		return "\t".join([str(self.PC), str(self.predictedPC), str(self.branchPredict)])
+		
+	#method to fill in fields of the BTBEntry 
+	def fillEntry(self, PC):
+		self.PC = PC
+		#not worrying about assigning predictedPC here as that will be done when updatePrediction is called right after this
+		#self.predictedPC = 0
+		self.branchPredict = 0 #doesn't matter what we set this to, it will be updated directly after populating entry
+
+	#method to update the predicted branch result
+	def updatePrediction(self, branchResult, offset, mode):
+		#check the current 1-bit predictor state
+		if self.branchPredict == 1 and branchResult == 0: #if branch was predicted taken, but actually wasn't, update
+			self.branchPredict = 0
+			#also update the predicted PC to reflect this change
+			return self.calculatePredictedPC(offset, mode)
+		elif self.branchPredict == 0 and branchResult == 1: #if branch was predicted not taken, but actually wasn't, update
+			self.branchPredict = 1
+			#also update the predicted PC to reflect this change
+			return self.calculatePredictedPC(offset, mode)
+		#don't need to worry about the other cases since it's only a 1-bit predictor 
+
+	#calculate the predicted PC - called upon filling BTBEntry, as well as when changing the prediction bit
+	def calculatePredictedPC(self, offset, mode):
+		#mode 1 for calculating: byte address calculation - only including this for if the professor uses byte addressed PC
+		if mode == 1:
+			#check if branch is predicted to be taken or not, and update predicted PC accordingly
+			if self.branchPredict == 1: #branch is predicted to be taken
+				#new PC = PC+4+offset<<2
+				bytePC = ((self.PC*4) + 4 + int(offset)) << 2 #byte addressed PC
+				self.predictedPC = bytePC / 4 #convert back to "regular" notation by dividing by 4, the number of bytes per PC entry
+			else: #else branch is predicted to be NOT taken
+				#new PC = PC+4
+				self.predictedPC = self.PC + 1 #not gonna bother with adding 4 then div by 4
+		else: #else, can just use normal "relative" addressing, ie PC goes 0,1,2,3,4,5,etc
+			#check if branch is predicted to be taken or not, and update predicted PC accordingly
+			if self.branchPredict == 1: #branch is predicted to be taken
+				#new PC = PC+1+offset
+				self.predictedPC = self.PC + 1 + int(offset) 
+				#print("self.predictedPC: ", self.predictedPC)
+			else: #else branch is predicted to be NOT taken
+				#new PC = PC + 1
+				self.predictedPC = self.PC + 1 
+				
+		return self.predictedPC
+			
+				
+		#not going to do the byte addressing [PC+4+offset<<2] method, just relative
+		#e.g. going from instr 7 to 0 -> 7+1-8=0, can use offset directly
+
+	#set the predicted PC for this entry
+	def setPredictedPC(self, newPC):
+		self.predictedPC = newPC
+
+	#grab PC for this entry
+	def getPC(self):
+		return self.PC
+		
+	#grab predicted PC for this entry
+	def getPredictedPC(self):
+		return self.predictedPC
+		
+	#grab the prediction for this branch - taken or not taken
+	def getPrediction(self):
+		return self.branchPredict   
         
 
 class BTB:
@@ -86,7 +88,7 @@ class BTB:
         
     #method to update the branch prediction of the given entry
     def updateEntryPrediction(self, entry, result, offset, mode):
-        self.entries[entry].updatePrediction(result, offset, mode)
+        return self.entries[entry].updatePrediction(result, offset, mode)
         
     #grab the address/PC for a given entry
     def getEntryPC(self, entry):
@@ -116,12 +118,12 @@ class BranchPredictor:
         entry = self.getEntry(PC)
         #if this PC is in the BTB, just update the related branch prediction bit
         if self.BTB.getEntryPC(entry) == PC:
-            self.BTB.updateEntryPrediction(entry, result, offset, mode)
+            return self.BTB.updateEntryPrediction(entry, result, offset, mode)
         else:
             #else, this PC must be added to the BTB as its own entry
             self.BTB.populateEntry(entry, PC)
             #also update the branch result in the event of not taken, since the BTB entry initializes to taken
-            self.BTB.updateEntryPrediction(entry, result, offset, mode)
+            return self.BTB.updateEntryPrediction(entry, result, offset, mode)
             
     #method to return the listed PC of a branch
     def getEntryPC(self, PC):
